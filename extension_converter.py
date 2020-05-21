1614dcea08f1419ccc408772494e32dc39232ef1
cdacdsjuhu <63604282+cdacdsjuhu@users.noreply.github.com>
2020-05-21 14:05:16 +0530

    Update extension_converter.py

--- extension_converter.py
+++ extension_converter.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import glob
-extensions= ['png', 'jpeg', 'gif', 'svg']
+extensions= [ 'jpeg', 'png', 'ppm', 'bmp', 'pgm', 'tiff', 'bmp', 'JPEG', 'PNG', 'PPM', 'BMP', 'PGM', 'TIFF', 'BMP']
 count=0
 extenlist=[]
 for exten in extensions:
-    for file in glob.glob("F:\\test_extension_image\*.%s"%(exten)):
+    for file in glob.glob(r"C:\Users\bhatiya\Desktop\LockdownWorkdone\renaming_testing\Kissing_ver\*.%s"%(exten)):
         print(file)
-        name= file[24:]
+        name= file[71:]
         extenlist.append(name)
         im = Image.open(file)
         #print(im)
@@ -18,6 +18,6 @@ for exten in extensions:
 
 print("count is " + str(count))
 
-with open(r'C:\Users\bhatiya\Desktop\LockdownWorkdone\json\extension.txt', 'w') as filehandle:
+with open(r'C:\Users\bhatiya\Desktop\LockdownWorkdone\renaming_testing\extension.txt', 'w') as filehandle:
     for listitem in extenlist:
         filehandle.write('%s\n' % listitem)
