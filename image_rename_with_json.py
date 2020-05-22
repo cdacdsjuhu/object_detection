90180748abad464d5818c3986287e9d56cc7e299
cdacdsjuhu <63604282+cdacdsjuhu@users.noreply.github.com>
2020-05-22 10:53:11 +0530

    Update image_rename_with_json.py

--- image_rename_with_json.py
+++ image_rename_with_json.py
@@ -1,19 +1,35 @@
 import pickle
-import os
-work_dir= r'C:\Users\bhatiya\Desktop\LockdownWorkdone\renaming_testing\Kissing_ver'
-namelist = pickle.load( open( r"C:\Users\bhatiya\Desktop\LockdownWorkdone\renaming_testing\save.p", "rb" ) )
-print(namelist)
-keys = namelist.keys()
-print(keys)
-images= os.listdir(work_dir)
-cnt=0
-for image in images:
-    if image in keys:
-        image_files1 = namelist[image] +".jpg"
-        os.rename(work_dir + "\\" + image, work_dir + "\\" + image_files1)
-        print("{} file renamed into {}".format(image, image_files1))
-        print()
+import json
+json_file = r'/home/ayushi/sad_data/kissing/sexual_part_detection_kissing.json'
+count= 0
+name_dict= {}
+with open(json_file, 'r+') as file:
+    flag_dict = {}
 
-        cnt += 1
+    json_data = json.load(file)
 
-print("total {} files renamed..".format(cnt))
+    via_img_metadata = json_data['_via_img_metadata']
+
+    for (image_id, attr) in via_img_metadata.items():
+
+        filename = attr['filename']
+        #print( type(filename2))
+        print(' workking on file ' + filename)
+
+        newname= str(count)+'_kiss_'+".jpg"
+        name_dict[filename]= newname
+        size= attr['size']
+        print(type(size))
+        image_idnew = str(count)+'_kiss_'+".jpg"+ str(size)
+        attr['filename'] = newname
+        via_img_metadata[image_idnew] = via_img_metadata[image_id]
+        del via_img_metadata[image_id]
+        count=count+1
+
+
+with open(json_file, 'w') as jsonFile:
+    json.dump(json_data, jsonFile)
+
+print(name_dict)
+
+pickle.dump( name_dict, open(r"/home/ayushi/sad_data/kissing/kissing_save.p", "wb" ) )
